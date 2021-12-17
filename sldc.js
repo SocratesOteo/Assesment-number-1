1f1abb81af8a96bd6021e2d1a782ff9c5cce7450
Paloma Oteo <oteokids@gmail.com>
2021-12-17 09:47:27 -0700

    sldc

--- sldc.js
+++ sldc.js
@@ -12,6 +12,15 @@
 // Planning
 // Testing & Integration
 
+//Real Order:
+/*
+planning
+analysis of requirements
+design
+testing and integration
+implementation
+maintain
+*/
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +28,23 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysis = "Determining what the applicaton is used for and what is required in the application or project"
+
+const design = "Modeling in which way the application or software will be used and how it will work"
+
+const testing = " Making sure the bugs are all sorted out and that its been proven to work completely"
+
+const implementation = "At this point the project is made public for everybody and should be serving its purpose"
+
+const maintain = "users might find bug that needs to be fixed or developers might want to shange or add things to the project"
+
+
+
+
+
+
+
+
+
+
